42f2538539b9fad55a8e38a5b0194e181ae826f2
Arun Prabhakar <arunprabhakar121995@gmail.com>
2025-05-22 00:18:49 +0530

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,16 @@ const { typeDefs, resolvers } = require("./graphql");
 async function startServer() {
   await connectToDb();
 
-  const server = new ApolloServer({ typeDefs, resolvers });
-
-  server.listen({ port: 4000 }).then(({ url }) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    cors: {
+      origin: ["http://localhost:3000", "https://your-frontend.vercel.app"], // adjust as needed
+      credentials: true,
+    },
+  });
+  const PORT = process.env.PORT;
+  server.listen({ port: PORT }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
   });
 }
